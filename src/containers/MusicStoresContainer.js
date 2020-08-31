0b1352914ff53100b8bc99dcc0f285981bcee2b5
zdsmi <zdsmith001@gmail.com>
2020-08-31 12:45:03 -0500

    added mapStateToProps and mapDispatchToProps for MusicStoreContainer

--- src/containers/MusicStoresContainer.js
+++ src/containers/MusicStoresContainer.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import { MusicStores } from '../components/MusicStores'
+import { MusicStoreInput } from '../components/MusicStoreInput'
 import { connect } from 'react-redux'
 
 class MusicStoresContainer extends Component {
-  render() {}
-
+  render() {
+    return (
+      <div>
+        <MusicStoreInput addMusicStore={addMusicStore}/>
+        <MusicStores/>
+      </div>
+    )
+  }
 }
 
-export default connect()(MusicStoresContainer)
+const mapStateToProps = state => (
+  {musicStores: musicStores}
+)
+
+const mapDispatchToProps = dispatch => ({
+  addMusicStore: text => dispatch({type: 'ADD_MUSIC_STORE', text})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MusicStoresContainer)
