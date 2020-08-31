50fc4b87d0805a62d239c3352a9e7e71f1f2f57d
zdsmi <zdsmith001@gmail.com>
2020-08-31 11:20:00 -0500

    added starting code and connect function to MusicStoresContainer

--- src/containers/MusicStoresContainer.js
+++ src/containers/MusicStoresContainer.js
@@ -1,3 +1,10 @@
 import React, { Component } from 'react'
 import { MusicStores } from '../components/MusicStores'
 import { connect } from 'react-redux'
+
+class MusicStoresContainer extends Component {
+  render() {}
+
+}
+
+export default connect()(MusicStoresContainer)
