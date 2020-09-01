3cdca868160f0472009447fbc846d34604696963
zdsmi <zdsmith001@gmail.com>
2020-09-01 16:05:46 -0500

    added 'LOADING_MUSIC_STORES' and 'ADD_MUSIC_STORES' cases to reducer, changed server to port 3001 in package.json

--- src/reducers/manageMusicStores.js
+++ src/reducers/manageMusicStores.js
@@ -1,10 +1,22 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-//creates an initial state with keys for music stores and instruments
-export default function ManageMusicStores(state={ musicStores: [], instruments: [] }, action) {
+//creates an initial state with keys for music stores and instruments, as well as a boolean to indicate whether it is currently loading
+export default function ManageMusicStores(state={ musicStores: [], instruments: [], loading: false }, action) {
    switch (action.type) {
+    case 'LOADING_MUSIC_STORES':
+     return {
+      ...state,
+      musicStores: [...state.musicStores],
+      loading: true
+    }
+    case 'ADD_MUSIC_STORES':
+      return {
+         ...state,
+         musicStores: action.musicStores,
+         loading: false
+      }
     default:
-      return state
+     return state
    }
 }
