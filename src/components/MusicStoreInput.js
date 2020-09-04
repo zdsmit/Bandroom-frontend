49c3aeb36698e5cb76f57d343131dfbf02444363
zdsmi <zdsmith001@gmail.com>
2020-09-04 12:26:53 -0500

    changed music store input form from stateless to class component

--- src/components/MusicStoreInput.js
+++ src/components/MusicStoreInput.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const MusicStoreInput = (props) => {
+class MusicStoreInput extends Component {
   
   //on submitting the form, addMusicStore, passed down as a prop from the container, is used to send an 'add' action to the reducer
-  const handleOnSubmit = event => {
+  handleOnSubmit = event => {
     event.preventDefault()
     props.addMusicStore(event.target.value)
   }
   
-  return (
-    <div className="inputForm">
-      <h3>Create a new music store!</h3>
-      {/* input form contains inputs for all fields specified in the backend database's "stores" table (except instruments, which is handled separately on the frontend side) */}
-      <form onSubmit={event => handleOnSubmit(event)}>
-        <input type="text" placeholder="Store's Name"/><br/>
-        <input type="text" placeholder="Owner's Name"/><br/>
-        <input type="text" placeholder="Store's Location"/><br/>
-        <input type="submit" value="submit"/>
-      </form>
-    </div>
-  )
+  render() {
+    return (
+      <div className="inputForm">
+        <h3>Create a new music store!</h3>
+        {/* input form contains inputs for all fields specified in the backend database's "stores" table (except instruments, which is handled separately on the frontend side) */}
+        <form onSubmit={event => this.handleOnSubmit(event)}>
+          <input type="text" placeholder="Store's Name"/><br/>
+          <input type="text" placeholder="Owner's Name"/><br/>
+          <input type="text" placeholder="Store's Location"/><br/>
+          <input type="submit" value="submit"/>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default MusicStoreInput
