dbb81a1f4439080709ce8b6eb79f3861339446f9
zdsmi <zdsmith001@gmail.com>
2020-09-05 12:17:03 -0500

    added onChange functions for store name, owner, and location

--- src/components/MusicStoreInput.js
+++ src/components/MusicStoreInput.js
@@ -5,13 +5,29 @@ class MusicStoreInput extends Component {
   constructor() {
     super()
     this.state = {
-      musicStore: {
-
-      }
+      name: '',
+      owner: '',
+      location: ''
     }
   }
 
-  handleNameChange = 
+  handleNameChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOwnerChange = event => {
+    this.setState({
+      owner: event.target.value
+    })
+  }
+
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    })
+  }
   
   //on submitting the form, addMusicStore, passed down as a prop from the container, is used to send an 'add' action to the reducer
   handleOnSubmit = event => {
@@ -25,9 +41,9 @@ class MusicStoreInput extends Component {
         <h3>Create a new music store!</h3>
         {/* input form contains inputs for all fields specified in the backend database's "stores" table (except instruments, which is handled separately on the frontend side) */}
         <form onSubmit={event => this.handleOnSubmit(event)}>
-          <input type="text" placeholder="Store's Name"/><br/>
-          <input type="text" placeholder="Owner's Name"/><br/>
-          <input type="text" placeholder="Store's Location"/><br/>
+          <input type="text" placeholder="Store's Name" onChange={this.handleNameChange}/><br/>
+          <input type="text" placeholder="Owner's Name" onChange={this.handleOwnerChange}/><br/>
+          <input type="text" placeholder="Store's Location" onChange={this.handleLocationChange}/><br/>
           <input type="submit" value="submit"/>
         </form>
       </div>
